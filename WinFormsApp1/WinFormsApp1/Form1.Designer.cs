adf1d502f0c3d50c9768dc22781cdbcaae54b36b
bogdi xameli <121572305+gmsugff@users.noreply.github.com>
2024-12-23 20:47:13 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ControlDarkDark;
+            button1.Image = Properties.Resources._1679761337_36_ct4v;
+            button1.Location = new Point(73, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(235, 54);
+            button1.TabIndex = 0;
+            button1.Text = "ФИО";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.InactiveCaption;
+            button2.ForeColor = SystemColors.MenuHighlight;
+            button2.Location = new Point(73, 35);
+            button2.Name = "button2";
+            button2.Size = new Size(235, 58);
+            button2.TabIndex = 1;
+            button2.Text = "Время";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.Window;
+            button3.Location = new Point(73, 221);
+            button3.Name = "button3";
+            button3.Size = new Size(235, 54);
+            button3.TabIndex = 2;
+            button3.Text = "Дата рождения";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.WindowFrame;
+            button4.Location = new Point(73, 311);
+            button4.Name = "button4";
+            button4.Size = new Size(235, 51);
+            button4.TabIndex = 3;
+            button4.Text = "Все";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Info;
+            textBox1.Location = new Point(314, 130);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(657, 54);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(390, 221);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 5;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ButtonFace;
+            button5.Location = new Point(73, 432);
+            button5.Name = "button5";
+            button5.Size = new Size(235, 58);
+            button5.TabIndex = 6;
+            button5.Text = "начало игры";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(765, 237);
+            button6.Name = "button6";
+            button6.Size = new Size(183, 58);
+            button6.TabIndex = 7;
+            button6.Text = "кнопка";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(73, 523);
+            button7.Name = "button7";
+            button7.Size = new Size(235, 57);
+            button7.TabIndex = 8;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1173, 622);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+    }
+}
